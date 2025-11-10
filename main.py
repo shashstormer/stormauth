ad554d2d92eb50c06b6e81ee7beb4e1f66ed7c93
Shashank A Patil <shashanka5398@gmail.com>
2025-11-10 17:40:34 +0530

    FEAT: Implement custom theme settings for dark mode in FastAPI application

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from fastapi import FastAPI, Depends
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
-from authtuna import init_app
+from authtuna import init_app, Theme, init_settings, ThemeMode, settings
 from authtuna.integrations import get_current_user_optional
-
 app = FastAPI()
+new_theme = settings.THEME.dark.model_copy(deep=True)
+new_theme.background_start = "#143497"
+new_theme.background_end = "#000000"
+custom_theme = Theme(
+    mode="single", # only light mode vars but just set them to whatever you want they will be used in dark mode also.
+    light=new_theme,
+)
 
+# Override settings with custom theme
+init_settings(THEME=custom_theme, dont_use_env=False)
 init_app(app)
 app.add_middleware(CORSMiddleware, allow_origins=[
     "https://timeline.shashstorm.in"

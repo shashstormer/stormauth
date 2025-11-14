aaf661a206842c8402d1f57401514ea7df344d9a
Shashank A Patil <shashanka5398@gmail.com>
2025-11-14 12:47:09 +0530

    FEAT: Add lifecycle management for organization creation roles in FastAPI application

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
+from contextlib import asynccontextmanager
 from fastapi import FastAPI, Depends
 from fastapi.responses import RedirectResponse
 from fastapi.middleware.cors import CORSMiddleware
 from authtuna import init_app, Theme, init_settings, ThemeMode, settings
-from authtuna.integrations import get_current_user_optional
-app = FastAPI()
+from authtuna.integrations import get_current_user_optional, auth_service
+
+
+@asynccontextmanager
+async def lifecycle(app: FastAPI):
+    # Other people if you want to allow anyone to create organizations you can
+    # await auth_service.roles.add_permission_to_role("User", "org:create", "system")
+    await auth_service.roles.get_or_create("OrgCreate", defaults={"description": "Users with this role can create organizations"})
+    await auth_service.roles.add_permission_to_role("OrgCreate", "org:create", "system")
+    await auth_service.roles.grant_relationship(granter_role_name="System", grantable_name="OrgCreate", grantable_manager=auth_service.roles, relationship_attr="can_assign_roles")
+    user = await auth_service.users.get_by_email("shashanka5398@gmail.com")
+    if user:
+        await auth_service.roles.assign_to_user(user.id, "OrgCreate", "system", "global")
+    yield
+
+app = FastAPI(docs_url=None, redoc_url=None, lifespan=lifecycle)
 new_theme = settings.THEME.dark.model_copy(deep=True)
 new_theme.background_start = "#152b72"
 new_theme.background_end = "#081638"
